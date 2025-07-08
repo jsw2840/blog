956eede69389364e0e0f669f1af48d93ab4646ed
seoungwon <jis2843@naver.com>
2025-07-08 17:29:27 +0900

    http postman 실습(get, post, put, delete)

---
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,39 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// 사용자가 요청 -> 응답(HTML 파일)
+// @Controller
+
+// 사용자가 요청 -> 응답(Data)
+@RestController
+public class HttpControllerTest {
+	
+	//인터넷브라우저요청은 무조건 get요청밖에 할 수 없다.
+	//http://localhost:8080/http/get(select)
+	@GetMapping("/http/get")
+	public String getTest(@RequestParam Member m) {//id=1&username=ssar&password=1234&email=ssar@nate.com//MessageConverter(스프링부트)
+		return "get요청 :" + m.getId() +","+ m.getUsername()+","+ m.getPassword()+","+m.getEmail();
+	}
+	//http://localhost:8080/http/post(insert)
+	@PostMapping("/http/post")// text/plain, application/json
+	public String postTest(@RequestBody Member m) { //MessageConverter(스프링부트)
+		return "post요청 :" + m.getId() +","+ m.getUsername()+","+ m.getPassword()+","+m.getEmail();
+	}
+	//http://localhost:8080/http/put(update)
+	@PutMapping("/http/put")
+	public String putTest(@RequestBody Member m) {
+		return "put요청 :" + m.getId() +","+ m.getUsername()+","+ m.getPassword()+","+m.getEmail();
+	}
+	//http://localhost:8080/http/delete(delete)
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete요청";
+	}
+}
